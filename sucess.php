0c2222980fcbeb37818d7496953d6a130be8b9b0
amosmugendi <amosmugendi07@gmail.com>
2023-05-22 14:46:04 +0300

    updated
    updated

--- sucess.php
+++ sucess.php
@@ -3,32 +3,38 @@
         require_once 'includes/header.php';
         require_once 'db/conn.php';
         require_once 'sendmail.php';
-
-        if(isset($_POST['submit'])){
-            //extract values from the $_POST array
-            $fname=$_POST['firstname'];
-            $lname=$_POST['lastname'];
-            $dob=$_POST['dob'];
-            $email=$_POST['email'];
-            $contact=$_POST['phone'];
-            $specialty=$_POST['specialty'];
-            //CALL FUNCTIONto isnert and track if success or not
-            $isSuccess= $crud->insertAttendees($fname,$lname,$dob,$email,$contact,$specialty);
-            $specialtyName= $crud->getSpecialtyById($specialty);
-            if($isSuccess){
-               SendEmail::SendMail($email, 'Welco to IT conference' , 'You have sucessfully been registered');
-
-              include('includes/sucessmessage.php') ;
+        if (isset($_POST['submit'])) {
+            // Extract values from the $_POST array
+            $fname = $_POST['firstname'];
+            $lname = $_POST['lastname'];
+            $dob = $_POST['dob'];
+            $email = $_POST['email'];
+            $contact = $_POST['phone'];
+            $specialty = $_POST['specialty'];
+        
+            // Check if the "avatar" key is set in the $_FILES array
+            if (isset($_FILES['avatar'])) {
+                $orig_file = $_FILES['avatar']['name'];
+                $target_dir = 'uploads/';
+                $destination = $target_dir . basename($_FILES['avatar']['name']);
+                move_uploaded_file($orig_file, $destination);
+            } else {
+                // Handle the situation when "avatar" key is not set
+                $destination = null; // or assign a default value
             }
-            else{
-               include 'includes/errormessage.php';
+            // CALL FUNCTION to insert and track if success or not
+            $isSuccess = $crud->insertAttendees($fname, $lname, $dob, $email, $contact, $specialty);
+            $specialtyName = $crud->getSpecialtyById($specialty);
+        
+            if ($isSuccess) {
+                SendEmail::SendMail($email, 'Welcome to IT conference', 'You have successfully been registered');
+                include('includes/successmessage.php');
+            } else {
+                include 'includes/errormessage.php';
             }
         }
-
-
-    ?>
-
-      
+        
+    ?>  
     <div class="card" style="width: 18rem">
         <div class="card-body">
             <h5 class="card-title">
